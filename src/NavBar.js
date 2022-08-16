77bb57ae57bc540367a97447a8ce4eacf53b7726
EldarK-NS <eldark.ns@gmail.com>
2022-08-16 11:21:06 +0500

    last

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,8 @@
-import { Link } from "@chakra-ui/react";
 import React from "react";
-import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Spacer, Link } from "@chakra-ui/react";
+import Facebook from "./assets/social-media-icons/facebook_32x32.png";
+import Twitter from "./assets/social-media-icons/twitter_32x32.png";
+import Email from "./assets/social-media-icons/email_32x32.png";
 
 const NavBar = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
@@ -14,10 +16,20 @@ const NavBar = ({ accounts, setAccounts }) => {
     }
   }
   return (
-    <div>
-      <div>Facebook</div>
-      <div>Twitter</div>
-      <div>Email</div>
+    <Flex justify='space-between' align='center' padding='30px'>
+      <Flex justify='space-around' width='40%' padding='0 75px'>
+        <Link href='https://www.facebook.com'>
+          <Image src={Facebook} boxSize='42px' margin='0 15px' />
+        </Link>
+
+        <Link href='https://www.twitter.com'>
+          <Image src={Twitter} boxSize='42px' margin='0 15px' />
+        </Link>
+
+        <Link href='https://www.gmail.com'>
+          <Image src={Email} boxSize='42px' margin='0 15px' />
+        </Link>
+      </Flex>
 
       <div>About</div>
       <div>Mint</div>
@@ -28,7 +40,7 @@ const NavBar = ({ accounts, setAccounts }) => {
       ) : (
         <button onClick={connectAccount}>Connect</button>
       )}
-    </div>
+    </Flex>
   );
 };
 
